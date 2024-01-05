1cc53bf903153d79155638b5eec274848d9883c1
eileen <emcnaughton@wikimedia.org>
2024-01-05 17:22:57 +1300

    Civix upgrade

--- testdata.php
+++ testdata.php
@@ -24,59 +24,12 @@ function testdata_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function testdata_civicrm_postInstall() {
-  _testdata_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function testdata_civicrm_uninstall() {
-  _testdata_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function testdata_civicrm_enable() {
   _testdata_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function testdata_civicrm_disable() {
-  _testdata_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function testdata_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _testdata_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function testdata_civicrm_entityTypes(&$entityTypes) {
-  _testdata_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
